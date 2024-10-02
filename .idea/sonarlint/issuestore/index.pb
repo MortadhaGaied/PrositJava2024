a2e4fcd04c3da4aa2f0e2b6a3d1fcb3b19d13ee6
MortadhaGaied <73293527+MortadhaGaied@users.noreply.github.com>
2024-10-02 18:16:47 +0100

    delete System.out.println("*******************");

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-?
-src/Animal.java,c\9\c9d1c15b1c4ef76bfd08d518b5e833a99f5f7b92
-<
-src/Zoo.java,2\b\2b4e2959fed6a875d3c3b99187ba64dfdc253ea8
-F
-src/ZooManagement.java,c\0\c0af9c271d30273909a1d91e6e81011825538b58
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -37,7 +37,6 @@ public class ZooManagement {
         String str2="Hello";
         String str3=new String("Hello");
         System.out.println(str1.equals(str3));
-        System.out.println("*******************");
         System.out.println(Animal.n);
 
 
